61ed14b62440c1b1c6fb52311af33fe9f2933dc4
sachinyadav3496 <sachinyadav3496@gmail.com>
2018-08-15 00:41:24 -0700

    added debit service

--- bank_app.py
+++ bank_app.py
@@ -60,53 +60,79 @@ class Bank:
         self.credframe.grid(row=4,column=0,padx=76,pady=25)
 
     def debframe(self):
+    
+        self.menu.grid_forget()
+        self.debit_Balance = tk.Frame(self.master,bg="#777777")
+        
+        self.up_amnt_lbl1 = Label(self.debit_Balance,text='Welcome {} to Debit Services'.format(self.user),font=('Times','20','bold'),fg="#ffffff",bg='#777777')
+        self.up_amnt_lbl1.grid(row=0,column=0,columnspan=2,pady=10,padx=30)
 
-        self.debframe = tk.Frame(self.menu,bg="#777777")
+        self.up_amnt_lbl = Label(self.debit_Balance,text='Enter amount to debit ',font=('Times','23','bold'),fg="#ffffff",bg='#777777')
+        self.up_amnt_lbl.grid(row=1,column=0,columnspan=2,pady=10,padx=30)
+        
+        self.up_amnt_lbl2 = Label(self.debit_Balance,text='Amount',font=('Times','20','bold'),fg="#ffffff",bg='#777777')
+        self.up_amnt_lbl2.grid(row=2,column=0,pady=10,padx=30)
+        
 
-        self.up_amnt_lbl = Label(self.debframe,text='Enter amount to debit ',bg="gray",font=('Times','30','bold'),fg="#00ff00")
-        self.up_amnt_lbl.grid(row=0,column=0,pady=10)
+        self.up_amnt = Entry(self.debit_Balance,bg='#123456',width=20,font=('Times','20','bold'),fg='#FFFFFF')
+        self.up_amnt.grid(row=2,column=1,pady=12,padx=55)
 
-        self.up_amnt = Entry(self.debframe,bg='#123456',width=20,font=('Times','20','bold'),fg='#FFFFFF')
-        self.up_amnt.grid(row=1,column=0,pady=10)
+        self.up_amnt_btn = tk.Button(self.debit_Balance,text='update',bg="#777777",font=('Times','20','bold'),command=self.update_balance,fg="#000000",width=12)
+        self.up_amnt_btn.grid(row=3,column=1,pady=16)
+        
+        self.up_amnt_btn1 = tk.Button(self.debit_Balance,text='<<Back',bg="#777777",font=('Times','18','bold'),command=self.show_m4,fg="#000000",width=10)
+        self.up_amnt_btn1.grid(row=4,column=0,pady=17,padx=40)
 
-        self.up_amnt_btn = tk.Button(self.debframe,text='update balance',bg="#777777",font=('Times','20','bold'),command=self.debit,fg="#aadcba")
-        self.up_amnt_btn.grid(row=2,column=0,pady=10)
 
-
-        self.debframe.grid(row=4,column=0,padx=76,pady=25)
-
+        self.debit_Balance.grid(padx=self.ws*.3,pady=self.hs*.2)
+    
+    def show_m4(self):
+        self.debit_Balance.grid_forget()
+        self.menu.grid(padx=self.ws*.3,pady=self.hs*.2)
 
 
     def credit(self):
         acc=Bank.data[0]
         amnt=self.up_amnt.get()
-
         cmd="update user SET balance=balance+{} where acc='{}'".format(amnt,acc)
         Bank.c.execute(cmd)
         Bank.db.commit()
-
         s='\n'+str(amnt)+' credited to the account  {} '.format(acc)
-
         messagebox.showinfo("CREDIT",s)
 
-    def debit(self):
-        acc=Bank.data[0]
-        amnt=float(self.up_amnt.get())
-        print('amnt:',amnt)
-        print('balance',Bank.data[3])
+    def update_balance(self):
+        if self.up_amnt.get() :
+            try :
+                amnt=float(self.up_amnt.get())
+                db = sql.connect('localhost','bank','bank','bank')
+                c = db.cursor()
+                c.execute('select balance from user where name="{}"'.format(self.user))
+                bal = c.fetchone()[0]
+                if bal :
+                    if amnt <= bal:
+                        
+                        cmd="update user SET balance=balance-{} where name='{}'".format(amnt,self.user)
+                        c.execute(cmd)
+                        db.commit()
+                        s='!!DEBITED SUCESSFULLY!! \n {}rs debited from Your Account\nYour Updated Balance is now {}.'.format(amnt,bal-amnt)
+                        messagebox.showinfo("!!Sucess!!",s)
+                    
+                    else:
+                        
+                        s='\nInsufficient account balance\nyou only have {}rs in your account'.format(bal)
+                        messagebox.showinfo("!!DEBIT ERROR!!",s)
 
+                    
+                else :
+                    messagebox.showerror("!!UNKOWN ERROR!!","DATABASE LOOKUP Error \nSomething WEnt Wrong")
+            except ValueError as e :
+                messagebox.showerror("!!Value Error!!","!!ERROR!!Enter Vaid Amount to Debit\n")
 
-        if amnt <= Bank.data[3]:
-            cmd="update user SET balance=balance-{} where acc='{}'".format(amnt,acc)
-            Bank.c.execute(cmd)
-            Bank.db.commit()
+            except Exception as error :
+                messagebox.showerror("!!DataBase Connectivity Error!!","!!ERROR!!{}".format(error))
 
-            s='\n'+str(amnt)+' debited from the account {} '.format(acc)
-
-        else:
-            s='\nInsufficient balance for the account {}\n'.format(acc)
-
-        messagebox.showinfo("Debit",s)
+        else :
+            messagebox.showerror("!!Input Error!!","Please Enter Amount to Debit")
 
 
     def show_f(self):
@@ -235,7 +261,7 @@ class Bank:
         self.new_password = Entry(self.passupdate,bg='#123456',textvariable=self.New_Password,width=20,show="*",font=('Times','20','bold'),fg='#FFFFFF')
         self.new_password.grid(row=2,column=1,padx=30,pady=21)
 
-        self.pass_b1=tk.Button(self.passupdate,text="Update",width=10,bg="gray",font=('Times','20','bold'),command=self.update_password,fg="#FFFFFF")
+        self.pass_b1=tk.Button(self.passupdate,text="Update",width=10,bg="gray",font=('Times','20','bold'),command=self.update_password,fg="#000000")
         self.pass_b1.grid(row=3,column=1,columnspan=2,padx=30,pady=22)
         
         self.pass_b2=tk.Button( self.passupdate,text="<<Back",bg="gray",font=('Times','18','bold'),command=self.show_m3,fg="#000000")
@@ -291,14 +317,9 @@ class Bank:
         self.nameupdate.grid(padx=self.ws*.3,pady=self.hs*.2)
 
     def show_m1(self):
-
         self.nameupdate.grid_forget()
         self.profframe.grid(padx=self.ws*.3,pady=self.hs*.2)
     
-    def show_m3(self):
-
-        self.passupdate.grid_forget()
-        self.profframe.grid(padx=self.ws*.3,pady=self.hs*.2)
 
 
     def main_frame(self):
